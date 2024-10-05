b784ef241ce85cc70284dd7829d5e7415c351d93
RensithUdara <rensithudaragonalagoda@gmail.com>
2024-10-06 01:21:23 +0530

    Default props

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import Product from './components/Products';
 function App() {
   return (
     <>
-    <h1>My Products</h1>
-    <Product name = "Apple" price = {150} />
-    <Product name = "Orange" price = {200}/>
-    <Product name = "Banana" price = {100}/>
-    <Product name = "Guava" price = {120}/>
-    <Product name = "Grapes" price = {430}/>    
+      <h1>My Products</h1>
+      <Product name="Apple" price={150} stats={
+        {
+          views: 20,
+          purchases: 10
+        }
+      } />
+      <Product name="Orange" price={200} />
+      <Product name="Banana" price={100} />
+      <Product name="Guava" price={120} />
+      <Product name="Grapes" price={430} />
     </>
 
   );
--- src/components/Products.js
+++ src/components/Products.js
@@ -1,8 +1,11 @@
-function Product ({ name , price}){
+function Product({ name, price, stats = {views : 0 , purchases : 0}}) {
     return (
         <div>
             <h3>{name}</h3>
             <p>Rs.{price}</p>
+
+            <p>{stats.views} views</p>
+            <p>{stats.purchases} purchases</p>
         </div>
     );
 }
